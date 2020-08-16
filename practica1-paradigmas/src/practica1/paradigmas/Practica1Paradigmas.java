07cc9be8f81e9645693a65457df06cc76d615ac7
Lea <lea.cardenas14@gmail.com>
2020-08-15 21:37:47 -0600

    paint

--- practica1-paradigmas/src/practica1/paradigmas/Practica1Paradigmas.java
+++ practica1-paradigmas/src/practica1/paradigmas/Practica1Paradigmas.java
@@ -11,13 +11,13 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Shape;
 import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.Rectangle2D;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.Scanner;
+import javafx.stage.WindowEvent;
 
 /**
  *
@@ -25,38 +25,110 @@ import java.util.Scanner;
  */
 public class Practica1Paradigmas {
     public static Integer count = 0;
-    public VentanaSimple vista = null;
     public Boolean running = Boolean.FALSE;
     List<figure> figures = new ArrayList<>();
     
+    float[] b = Color.blue.getColorComponents(null);
+    Color blue = new Color(b[0], b[1], b[2], 0.35f); //circles
+    
+    float[] p = Color.pink.getColorComponents(null);
+    Color pink = new Color(p[0], p[1], p[2], 0.35f); //squares
+    
+    float[] pur = Color.MAGENTA.getColorComponents(null);
+    Color purple = new Color(pur[0], pur[1], pur[2], 0.35f); //rectangles
+    
+    float[] g = Color.green.getColorComponents(null);
+    Color green = new Color(g[0], g[1], g[2], 0.35f); //triangles
+    
+    float[] or = Color.ORANGE.getColorComponents(null);
+    Color orange = new Color(or[0], or[1], or[2], 0.35f); //donuts
     
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        new Practica1Paradigmas().empezarApp();
+        //new Practica1Paradigmas().empezarApp();
+        new Practica1Paradigmas().testing();
     }
     
-    public static class CircleDraw extends Frame {
-        Shape circle = new Ellipse2D.Float(100.0f, 100.0f, 100.0f, 100.0f);
+    public void testing(){
+        figures.add(new circle("Circle", 0, 200f, 200f, 50f));
+        figures.add(new square("Square", 3, 300f, 300f, 30f));
+        figures.add(new rectangle("Rectangle", 4, 400f, 400f, 50f, 40f));
+        figures.add(new donut("Donut", 5, 500f, 500f, 200f, 200f));
         
-        public void paint(Graphics g) {
-            Graphics2D ga = (Graphics2D)g;
-            ga.setColor(Color.red);
-            ga.draw(circle);
-            ga.fill(circle);
+        Frame frame = new printFigures();
+        frame.addWindowListener(new WindowAdapter(){
+            public void windowClosing(WindowEvent we){ System.exit(0); }
+        });
+        
+        frame.setSize(1000, 1000);
+        frame.setVisible(true);
+    }
+    
+    public class printFigures extends Frame {
+        public void paint(Graphics g){
+            for(figure f : figures){
+                Graphics2D ga = (Graphics2D)g;
+                switch(f.type){
+                    case "Circle":
+                        circle c = (circle) f;
+                        Shape circle = new Ellipse2D.Float(f.x, f.y, c.radio, c.radio);
+                        ga.setColor(blue);
+                        ga.draw(circle);
+                        ga.fill(circle);
+                        
+                        break;
+                    case "Square":
+                        square s = (square) f;
+                        Shape square = new Rectangle2D.Float(s.x, s.y, s.side, s.side);
+                        ga.setColor(pink);
+                        ga.draw(square);
+                        ga.fill(square);
+                        
+                        break;
+                    case "Rectangle":
+                        rectangle r = (rectangle) f;
+                        Shape rectangle = new Rectangle2D.Float(r.x, r.y, r.width, r.height);
+                        ga.setColor(purple);
+                        ga.draw(rectangle);
+                        ga.fill(rectangle);
+                        
+                        break;
+                    case "Triangle":
+                        triangle t = (triangle) f;
+                        //Rach
+//                    ga.setColor(green);
+//                    ga.draw(t);
+//                    ga.fill(t);
+                        
+                        break;
+                    case "Donut":
+                        donut d = (donut) f;
+                        Shape donut_1 = new Ellipse2D.Float(d.x, d.y, d.radio_1, d.radio_1);
+                        Shape donut_2 = new Ellipse2D.Float(d.x + 25f, d.y + 25f, d.radio_2, d.radio_2);
+                        ga.setColor(orange);
+                        ga.draw(donut_1);
+                        ga.fill(donut_1);
+                        
+                        ga.setColor(Color.white);
+                        ga.draw(donut_2);
+                        ga.fill(donut_2);
+                        break;
+                }
+            }
         }
     }
     
     //Shape circle = new Ellipse2D.Float(100.0f, 100.0f, 100.0f, 100.0f); x, y, r, r
-    //Shape square = new Rectangle2D.Double(100, 100,100, 100); x, y, w, h
+    //Shape square = new Rectangle2D.Float(100, 100,100, 100); x, y, w, h
     
     //System.out.print("");
     
     /*
     Circle: new Ellipse2D.Float(100.0f, 100.0f, 100.0f, 100.0f); x, y, r (r,r)
-    Square: new Rectangle2D.Double(100, 100,100, 100); x, y, l (l,l)
-    Rectangle: new Rectangle2D.Double(100, 100,100, 100); x, y, w, h
+    Square: new Rectangle2D.Float(100, 100,100, 100); x, y, l (l,l)
+    Rectangle: new Rectangle2D.Float(100, 100,100, 100); x, y, w, h
     Triangle:
     Donut: new Ellipse2D.Float(100.0f, 100.0f, 100.0f, 100.0f); x, y, r1 / new Ellipse2D.Float(100+25.0f, 100+25.0f, 100.0f, 100.0f); x, y, r2
     */
@@ -73,35 +145,35 @@ public class Practica1Paradigmas {
             switch(stringarray[0]){
                 case "circle":
                     if(cantArg(stringarray.length -1, 3)){
-                        figures.add(new circle("Circle",count,Double.valueOf(stringarray[1]), Double.valueOf(stringarray[2]), Double.valueOf(stringarray[3])));
+                        figures.add(new circle("Circle",count,Float.valueOf(stringarray[1]), Float.valueOf(stringarray[2]), Float.valueOf(stringarray[3])));
                     }else{
                         System.out.print("Error: Invalid quantity of arguments");
                     }
                     break;
                 case "square":
                     if(cantArg(stringarray.length -1, 3)){
-                        figures.add(new square("Square",count,Double.valueOf(stringarray[1]), Double.valueOf(stringarray[2]), Double.valueOf(stringarray[3])));
+                        figures.add(new square("Square",count,Float.valueOf(stringarray[1]), Float.valueOf(stringarray[2]), Float.valueOf(stringarray[3])));
                     }else{
                         System.out.print("Error: Invalid quantity of arguments");
                     }
                     break;
                 case "rectangle":
                     if(cantArg(stringarray.length -1, 4)){
-                        figures.add(new rectangle("Rectangle",count,Double.valueOf(stringarray[1]), Double.valueOf(stringarray[2]), Double.valueOf(stringarray[3]), Double.valueOf(stringarray[4])));
+                        figures.add(new rectangle("Rectangle",count,Float.valueOf(stringarray[1]), Float.valueOf(stringarray[2]), Float.valueOf(stringarray[3]), Float.valueOf(stringarray[4])));
                     }else{
                         System.out.print("Error: Invalid quantity of arguments\n Try again\n");
                     }
                     break;
                 case "triangle":
                     if(cantArg(stringarray.length -1, 6)){
-                        figures.add(new triangle("Triangle",count,Double.valueOf(stringarray[1]), Double.valueOf(stringarray[2]), Double.valueOf(stringarray[3]), Double.valueOf(stringarray[4]), Double.valueOf(stringarray[5]), Double.valueOf(stringarray[6])));
+                        figures.add(new triangle("Triangle",count,Float.valueOf(stringarray[1]), Float.valueOf(stringarray[2]), Float.valueOf(stringarray[3]), Float.valueOf(stringarray[4]), Float.valueOf(stringarray[5]), Float.valueOf(stringarray[6])));
                     }else{
                         System.out.print("Error: Invalid quantity of arguments\n Try again\n");
                     }
                     break;
                 case "donut":
                     if(cantArg(stringarray.length -1, 4)){
-                        figures.add(new donut("Donut",count,Double.valueOf(stringarray[1]), Double.valueOf(stringarray[2]), Double.valueOf(stringarray[3]), Double.valueOf(stringarray[4])));
+                        figures.add(new donut("Donut",count,Float.valueOf(stringarray[1]), Float.valueOf(stringarray[2]), Float.valueOf(stringarray[3]), Float.valueOf(stringarray[4])));
                     }else{
                         System.out.print("Error: Invalid quantity of arguments\n Try again\n");
                     }
@@ -154,12 +226,6 @@ public class Practica1Paradigmas {
         return Objects.equals(args, cant); //Demasiados argumentos
     }
     
-    public void registrar(String linea) {
-        System.out.printf("\t%s%n", linea);
-        vista.registrarMensaje(linea);
-        
-    }
-    
     public void delete(Integer id){
         for(figure f : figures){
             if(f.id.equals(id)){
@@ -174,8 +240,8 @@ public class Practica1Paradigmas {
     public class figure extends Frame {
         public String type;
         public Integer id;
-        public Double x;
-        public Double y;
+        public Float x;
+        public Float y;
         
         public void print(){
             System.out.print("Figure # " + id + " - Type: " + type + " Coords: (" + x + "," + y + ")\n");
@@ -183,9 +249,9 @@ public class Practica1Paradigmas {
     }
     
     public class circle extends figure{
-        public Double radio;
+        public Float radio;
         
-        public circle(String type, Integer id, Double x, Double y, Double radio) {
+        public circle(String type, Integer id, Float x, Float y, Float radio) {
             this.type = type;
             this.id = id;
             this.x = x;
@@ -196,9 +262,9 @@ public class Practica1Paradigmas {
     }
     
     public class square extends figure{
-        public Double side;
+        public Float side;
         
-        public square(String type, Integer id, Double x, Double y, Double side) {
+        public square(String type, Integer id, Float x, Float y, Float side) {
             this.type = type;
             this.id = id;
             this.x = x;
@@ -209,10 +275,10 @@ public class Practica1Paradigmas {
     }
     
     public class rectangle extends figure{
-        public Double width;
-        public Double height;
+        public Float width;
+        public Float height;
         
-        public rectangle(String type, Integer id, Double x, Double y, Double width, Double height) {
+        public rectangle(String type, Integer id, Float x, Float y, Float width, Float height) {
             this.type = type;
             this.id = id;
             this.x = x;
@@ -224,12 +290,12 @@ public class Practica1Paradigmas {
     }
     
     public class triangle extends figure{
-        public Double x_2;
-        public Double y_2;
-        public Double x_3;
-        public Double y_4;
+        public Float x_2;
+        public Float y_2;
+        public Float x_3;
+        public Float y_4;
         
-        public triangle(String type, Integer id, Double x, Double y, Double x_2, Double y_2, Double x_3, Double y_4) {
+        public triangle(String type, Integer id, Float x, Float y, Float x_2, Float y_2, Float x_3, Float y_4) {
             this.type = type;
             this.id = id;
             this.x = x;
@@ -243,10 +309,10 @@ public class Practica1Paradigmas {
     }
     
     public class donut extends figure{
-        public Double radio_1;
-        public Double radio_2;
+        public Float radio_1;
+        public Float radio_2;
         
-        public donut(String type, Integer id, Double x, Double y, Double radio_1, Double radio_2) {
+        public donut(String type, Integer id, Float x, Float y, Float radio_1, Float radio_2) {
             this.type = type;
             this.id = id;
             this.x = x;
